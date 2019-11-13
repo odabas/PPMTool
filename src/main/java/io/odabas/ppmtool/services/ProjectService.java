17d40acc60aee9e552f22fefb88f7a1b085c3bb1
Onur Odabas <tr.tc@msn.com>
2019-11-14 00:06:01 +0300

    Same projectId db error was handled by ControllerAdvice in new exception package

--- src/main/java/io/odabas/ppmtool/services/ProjectService.java
+++ src/main/java/io/odabas/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.odabas.ppmtool.services;
 
 import io.odabas.ppmtool.domain.Project;
+import io.odabas.ppmtool.exceptions.ProjectIdException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import io.odabas.ppmtool.repositories.ProjectRepository;
@@ -12,7 +13,12 @@ public class ProjectService  {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-
-        return  projectRepository.save(project);
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return  projectRepository.save(project);
+        }
+        catch (Exception ex){
+            throw  new ProjectIdException("ProjectId '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
     }
 }
--- src/main/java/io/odabas/ppmtool/web/ProjectController.java
+++ src/main/java/io/odabas/ppmtool/web/ProjectController.java
@@ -1,14 +1,22 @@
 package io.odabas.ppmtool.web;
 
 import io.odabas.ppmtool.domain.Project;
+import io.odabas.ppmtool.services.MapValidationErrorService;
 import io.odabas.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -17,9 +25,14 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
     @PostMapping
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project)
-    {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project , BindingResult result){
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap != null) return  errorMap;
+
         Project projectObj = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(project, HttpStatus.CREATED);
     }
