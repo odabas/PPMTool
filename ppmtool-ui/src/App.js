1c4c69ffb5850e2cc35a2c5704f6dc14f54bb82d
Onur Odabas <tr.tc@msn.com>
2019-11-17 02:15:54 +0300

    React project was created for frontend

---
+++ ppmtool-ui/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+      <Dashboard />
+      <Dashboard />
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
