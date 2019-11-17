86eefd1583afa6dce2411a121ed4cbf35dcb5a4e
Onur Odabas <tr.tc@msn.com>
2019-11-17 22:12:58 +0300

    Dashboard Navbar and ProjectItem modules was created.PPMToolDesign html design files was created

--- ppmtool-ui/src/App.js
+++ ppmtool-ui/src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
+import Header from "./components/Layout/Header";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
 function App() {
   return (
-    <div className="App">
-      <Dashboard />
-      <Dashboard />
-      <Dashboard />
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/addProject" component={AddProject} />
+      </div>
+    </Router>
   );
 }
 
